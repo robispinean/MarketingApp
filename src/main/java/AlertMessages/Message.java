d5c41b18891a2e815d75f6e3ed9b7073c907f79d
Robert Spinean <robispinean@yahoo.com>
2021-04-13 18:11:45 +0200

    added alert messages

---
+++ src/main/java/AlertMessages/Message.java
@@ -0,0 +1,12 @@
+package AlertMessages;
+
+import javafx.scene.control.Alert;
+
+public class Message {
+    public void setMessage(String str){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Alert Message");
+        alert.setContentText(str);
+        alert.show();
+    }
+}
