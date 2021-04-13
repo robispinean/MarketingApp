6bef593f1d253d79e57ac3e68dacd1ee3a67ba35
Robert Spinean <robispinean@yahoo.com>
2021-04-13 18:13:45 +0200

    added alert to hashpassword for errors and changed algorithm to SHA-256

--- src/main/java/Controllers/UserService.java
+++ src/main/java/Controllers/UserService.java
@@ -1,16 +1,21 @@
 package Controllers;
 
+import AlertMessages.Message;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class UserService {
-    String hashPassword(PasswordField p) {
+    Message msg = new Message();
+
+    String hashPassword(PasswordField p, String salt) {
         try {
-            // use PBKDF2 algorithm to engode the password
-            MessageDigest md = MessageDigest.getInstance("PBKDF2");
+            // use PBKDF2 algorithm to encode the password
+            final MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt.getBytes());
 
             byte[] messageDigest = md.digest(p.getText().getBytes());
 
@@ -23,7 +28,17 @@ public class UserService {
         }
         // handle missing/wrongly named algorithms
         catch (NoSuchAlgorithmException e) {
+            msg.setMessage("Something went wrong!");
             throw new RuntimeException(e);
         }
     }
+
+     boolean Validate(TextField email, PasswordField password){
+        String e = email.getText();
+        String p = password.getText();
+        if(e.length() >= 10 && e.length() <=30 && p.length() >= 6)
+            return true;
+        return false;
+    }
+
 }
