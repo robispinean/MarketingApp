15c4b83aeae13dc2deca13bd65c0ee8de4288682
Robert Spinean <robispinean@yahoo.com>
2021-04-25 16:28:27 +0200

    [MA-34] Created ArrayList for adding items to cart + some UI fixes

--- src/main/java/Controllers/HomePageController.java
+++ src/main/java/Controllers/HomePageController.java
@@ -1,10 +1,12 @@
 package Controllers;
 
 import Models.Item;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
@@ -17,6 +19,7 @@ import javafx.scene.text.Text;
 
 import java.awt.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -47,20 +50,30 @@ public class HomePageController implements Initializable{
     @FXML
     private HBox ItemLayout;
 
+    public Button AddToCart;
+
+
     private List<Item> items;
     private Listener clickListener;
+    private Item chosenItem;
+    private List<Item> cart = new ArrayList<>();
 
     private void setChosenItem(Item item) {
         ChosenItemName.setText(item.getName());
         ChosenItemPrice.setText(ItemController.CURRENCY + item.getPrice());
         try {
             System.out.println(item.getName() + " " + item.getImagePath());
-            String imagePath = "src/main/resources/img/item2.png";
             Image image = new Image(item.getImagePath());
             imageLeft.setImage(image);
+            chosenItem = new Item(item.getId(),item.getName(), item.getDescription(), item.getPrice(),item.getImagePath());
         }catch (Exception e){
             System.out.println(e);
         }
+    }
+
+    public void addItemToCart() {
+        cart.add(chosenItem);
+        System.out.println(cart);
     }
 
     @Override
@@ -87,7 +100,7 @@ public class HomePageController implements Initializable{
                     row++;
                 }
                 grid.add(ItemBox, column++, row);
-                GridPane.setMargin(ItemBox, new Insets(10));
+                GridPane.setMargin(ItemBox, new Insets(0));
             }
         }catch(Exception e){
             e.printStackTrace();
